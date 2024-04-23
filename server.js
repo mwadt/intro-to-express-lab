2bc14ad665d0e75227f77084874ac9c089e236a8
mwadt <mbrincko93@gmail.com>
2024-04-22 22:22:44 -0700

    added changes to the assignment

--- server.js
+++ server.js
@@ -1,29 +1,62 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const validator = require('validator');
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
 
 // Route to greet the user
 app.get('/greetings/:username', (req, res) => {
-    const { username } = req.params;
-    res.send(`Hello there, ${username}!`);
+    const username  = req.query.username;
+    res.send(`Hello there, ${req.params.username}!  What a delight to see you again!`);
 });
 
-// Route to roll the dice
+//Rolling the Dice Route
 app.get('/roll/:number', (req, res) => {
-    const { number } = req.params;
-
-    // Check if the parameter is a number
-    if (isNaN(number)) {
-        return res.send("You must specify a number.");
+    const number = req.params.number;
+    if(!validator.isNumeric(number)) {
+    res.send('You must specify a number');
+    };  
+    if(validator.isNumeric(number)) {
+        const randomNumber = Math.floor(Math.random() * req.params.number + 1);
+    res.send(`<h1>You rolled a ${randomNumber}</h1>`)
     }
+})
 
-    // Generate a random number between 0 and the given number
-    const randomNumber = Math.floor(Math.random() * (parseInt(number) + 1));
+//I want THAT one! Route
+app.get('/collectibles/:indexNumber', (req, res) => {
+    const collectibles = [
+        { name: 'shiny ball', price: 5.95 },
+        { name: 'autographed picture of a dog', price: 10 },
+        { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+      ];
+    if(req.params.indexNumber >0 && req.params.indexNumber < 3){
+        res.send(`So, you want the ${collectibles[req.params.indexNumber].name}?  For $${collectibles[req.params.indexNumber].price}, it can be yours!"`);
+    } else {
+        res.send('That Item is not in stock yet!  Check back soon!');
+    }
+    
 
-    res.send(`You rolled a ${randomNumber}.`);
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+//Filter shoes by query parameters
+app.get('/shoes/', (req, res) => {
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+const {minPrice, maxPrice, type} = req.query
+    let shoeFilter = shoes.filter(shoe => {
+        if(minPrice && shoe.price < req.query.minPrice) return false;
+        if(maxPrice && shoe.price > req.query.maxPrice) return false;
+        if(type && shoe.type !== type) return false;
+        return true;
+    })
+    res.send(shoeFilter);
+
+})
